f95206e6880c702e5c948aae746422f8d6a00677
eam95 <ericksonmijangos001@gmail.com>
2026-04-08 01:57:25 -0700

    Modified LED Adapter header and c file.

--- rc_car_integrated_II/Core/Inc/ledAdapter.h
+++ rc_car_integrated_II/Core/Inc/ledAdapter.h
@@ -9,10 +9,16 @@
 #define INC_LEDADAPTER_H_
 
 /* LED SPI Control */
-#define OFF_LED_EXT     0x00
-#define BLUE_LED_EXT    0x01
-#define YELLOW_LED_EXT  0x02
-#define GREEN_LED_EXT   0x04
-#define RED_LED_EXT     0x08
+#define OFF_LED_EXT            0x00
+#define LIGHT_GREEN_LED_EXT    0x01
+#define WHITE_LED_EXT          0x02
+#define PINK_LED_EXT           0x04
+#define BLUE_LED_EXT           0x08
+#define GREEN_LED_EXT          0x10
+#define YELLOW_LED_EXT         0x20
+#define ORANGE_LED_EXT         0x40
+#define RED_LED_EXT            0x80
+
+void shift_register_send(uint8_t data);
 
 #endif /* INC_LEDADAPTER_H_ */
